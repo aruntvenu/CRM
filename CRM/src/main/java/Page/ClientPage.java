7867dce934cc674bd61b6fd30dad7c41e8f137fd
arunt <arunt@LAPTOP-ABGB6O97>
2024-01-03 21:18:03 +0530

    test

--- CRM/src/main/java/Page/ClientPage.java
+++ CRM/src/main/java/Page/ClientPage.java
@@ -38,12 +38,12 @@ public class ClientPage {
 	@FindBy(xpath="//input[@name='vat_number']")
 	WebElement vatNumberField;
 	@FindBy(xpath="//div[@id='s2id_currency']")
-    WebElement currencyField;
+	WebElement currencyField;
 	@FindBy(xpath="//input[@id='s2id_autogen5_search']")
 	WebElement currencySearchField;
 	@FindBy(xpath="//span[text()='AFN']")
 	WebElement selectedCurrencyAFN;
-	
+
 	@FindBy(xpath="//button[text()=' Save']")
 	WebElement save;
 	@FindBy(xpath="//input[@type='search']")
@@ -68,69 +68,69 @@ public class ClientPage {
 	WebElement noRecordFound;
 	WaitUtility waitUtil;
 	ElementUtility elementUtil;//object declaration
-	
-	
-	
-	
+
+
+
+
 	public ClientPage(WebDriver driver) 
-	
+
 	{
 		// TODO Auto-generated constructor stub
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 		waitUtil=new WaitUtility(driver);
-	    elementUtil=new ElementUtility(driver); //object initialization
+		elementUtil=new ElementUtility(driver); //object initialization
 	}
-public String addClient(String companyName,String address,String city,String state,String zip,String country,String phone,String website,String vatNumber,String currency)
-{
-	
-	  clients.click();
-	  waitUtil.waitForClick(addClient);
-	  addClient.click();
-	  companyNameField.sendKeys(companyName);
-	  addressField.sendKeys(address);
-	  cityField.sendKeys(city);
-	  stateField.sendKeys(state);
-	  elementUtil.scroll(countryField);
-	  countryField.sendKeys(country);
-	  phoneField.sendKeys(phone);
-	  websiteField.sendKeys(website);
-	  vatNumberField.sendKeys(vatNumber);
-	  currencyField.click();
-	  currencySearchField.sendKeys(currency);
-	  selectedCurrencyAFN.click();
-	  //elementUtil.scroll(save);
-	  
-	  
-	  save.click();
-	  waitUtil.waitForClick(closeButton);
-	  closeButton.click();  
-	  finalClose.click();
-	  clientsSearch.sendKeys(companyName);
-	  String actual=clientCompanyNameColumn.getText();
-	  return actual;
-	  }
-public String searchAndEditClient(String searchValue,String editValue )
-{
-clients.click();
-waitUtil.waitForClick(clientsSearch);
-clientsSearch.sendKeys(searchValue);
-clientsEdit.click();
-//companyNameEdit.clear();
-//companyNameEdit.sendKeys(editValue);
-elementUtil.clearAndSendKey(companyNameEdit, editValue);
-companyNameEditSave.click();
-waitUtil.waitForClick(closeButton);
-closeButton.click();  
-finalClose.click();
-clientsSearch.clear();
-clientsSearch.sendKeys(editValue);
-String actual=clientCompanyNameColumn.getText();
-return actual;
-}
-public String searchAndDeleteClient(String searchValue)
-{   clients.click();
-waitUtil.waitForClick(clientsSearch);
+	public String addClient(String companyName,String address,String city,String state,String zip,String country,String phone,String website,String vatNumber,String currency)
+	{
+
+		clients.click();
+		waitUtil.waitForClick(addClient);
+		addClient.click();
+		companyNameField.sendKeys(companyName);
+		addressField.sendKeys(address);
+		cityField.sendKeys(city);
+		stateField.sendKeys(state);
+		elementUtil.scroll(countryField);
+		countryField.sendKeys(country);
+		phoneField.sendKeys(phone);
+		websiteField.sendKeys(website);
+		vatNumberField.sendKeys(vatNumber);
+		currencyField.click();
+		currencySearchField.sendKeys(currency);
+		selectedCurrencyAFN.click();
+		//elementUtil.scroll(save);
+
+
+		save.click();
+		waitUtil.waitForClick(closeButton);
+		closeButton.click();  
+		finalClose.click();
+		clientsSearch.sendKeys(companyName);
+		String actual=clientCompanyNameColumn.getText();
+		return actual;
+	}
+	public String searchAndEditClient(String searchValue,String editValue )
+	{
+		clients.click();
+		waitUtil.waitForClick(clientsSearch);
+		clientsSearch.sendKeys(searchValue);
+		clientsEdit.click();
+		//companyNameEdit.clear();
+		//companyNameEdit.sendKeys(editValue);
+		elementUtil.clearAndSendKey(companyNameEdit, editValue);
+		companyNameEditSave.click();
+		waitUtil.waitForClick(closeButton);
+		closeButton.click();  
+		finalClose.click();
+		clientsSearch.clear();
+		clientsSearch.sendKeys(editValue);
+		String actual=clientCompanyNameColumn.getText();
+		return actual;
+	}
+	public String searchAndDeleteClient(String searchValue)
+	{   clients.click();
+	waitUtil.waitForClick(clientsSearch);
 	clientsSearch.sendKeys(searchValue);
 	clientsDelete.click();
 	clientsDeleteConfirmation.click();
@@ -140,13 +140,13 @@ waitUtil.waitForClick(clientsSearch);
 	clientsSearch.sendKeys(searchValue);
 	String actual=noRecordFound.getText();
 	return actual;
-	
-}
-public String searchClient(String searchV)
-{
-	clients.click();
-	clientsSearch.sendKeys(searchV);
-	String actual=clientCompanyNameColumn.getText();
-	return actual;
-}
+
+	}
+	public String searchClient(String searchV)
+	{
+		clients.click();
+		clientsSearch.sendKeys(searchV);
+		String actual=clientCompanyNameColumn.getText();
+		return actual;
+	}
 }
